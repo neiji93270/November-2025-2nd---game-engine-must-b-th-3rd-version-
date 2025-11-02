0edeb1c9319a78608ab60b6518099ee759dceb3b
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-02 08:32:43 +0100

    Create Physicstographicsmergedoncou.h

---
+++ Physicstographicsmergedoncou.h
@@ -0,0 +1,12 @@
+//resolve topological simplex of some sort.
+//notice that we prevuously have five parameters 
+//the expansion of parameters in the simplex could be efficient in cpu.
+//but to keep video gale gonsoled aware of their cpus strength,
+//we have to sutdown cpu threads before acting on those both entities because
+//the heat transfer between them is not efficient.
+//as a result, it is bezt to not use any shitdown algorithms on gddrram of gtaphics cards.
+//a better way would be to hack direclty gpus. this is a loop ! so use a scheduller container inside
+//the gddrram. this container would have 20000 bots on one layer. this would prevent using ai and
+//then bios ai can act on gpus. this inceeqde graphic engine to 20 times more powerfull.
+//in reverse, increasing physic engine force, we need brute force.
+//
