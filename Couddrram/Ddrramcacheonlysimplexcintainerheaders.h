2820e90ce11746b36219c7ff35c3c016878933ab
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-02 12:54:42 +0100

    Create Ddrramcacheonlysimplexcintainerheaders.h

---
+++ Couddrram/Ddrramcacheonlysimplexcintainerheaders.h
@@ -0,0 +1,5 @@
+//the way to add a cache container are several.
+//we need a cache behaviour to enhance high energy systems with hilbert space. that techniques are well known
+//but the uninown cyber energy devices need a container to
+//add bits toncou and gpus necause hilbert space is not elegant at onc step.
+//so lets add some.stacks in the ddram cache.
