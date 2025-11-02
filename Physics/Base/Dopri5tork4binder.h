2f6469b866f716e59f6c74f4d2439176892c0eba
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-02 08:42:21 +0100

    Create Dopri5tork4binder.h

---
+++ Physics/Base/Dopri5tork4binder.h
@@ -0,0 +1,5 @@
+//enhancing the grzphics power by 20 could lead
+//to z better binding of rk4 from dopri5 methods. the simplew
+//then could need simplex methods for all devices. this is half of the full power of the console
+//and need esuations for it. models are not suited here for non power supply behaviour. then
+//fuve chips are needed for the simplex. so the bits travlling from cpu to other chips are unsafe.
